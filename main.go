abb102c6947fb4fcddeb2590476e929f329f4b5b
John <john@softsmart.co.za>
2024-09-17 16:33:58 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,123 @@
+// Remove images from S3 for past competitions.
+// If a competition is older than last month
+// then remove their images except for the
+// winners entries
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
+)
+
+type remoteConnection struct {
+	Active bool   `json:"active"`
+	Bucket string `json:"bucket"`
+}
+
+func getAwsConfig() aws.Config {
+	config, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		log.Fatal("Can't load AWS config")
+	}
+
+	return config
+}
+
+func deleteFolder(s3Client *s3.Client, bucket string, prefix string) error {
+
+	var continuationToken *string
+
+	for {
+		// List objects in the bucket with the given prefix
+		listObjectsInput := &s3.ListObjectsV2Input{
+			Bucket:            aws.String(bucket),
+			Prefix:            aws.String(prefix),
+			ContinuationToken: continuationToken,
+		}
+
+		listObjectsOutput, err := s3Client.ListObjectsV2(context.TODO(), listObjectsInput)
+		if err != nil {
+			return fmt.Errorf("failed to list objects: %w", err)
+		}
+
+		if len(listObjectsOutput.Contents) == 0 {
+			// No objects found, nothing to delete
+			return nil
+		}
+
+		// Prepare the objects to delete
+		var objectsToDelete []types.ObjectIdentifier
+		for _, object := range listObjectsOutput.Contents {
+			objectsToDelete = append(objectsToDelete, types.ObjectIdentifier{Key: object.Key})
+		}
+
+		// Delete objects
+		deleteObjectsInput := &s3.DeleteObjectsInput{
+			Bucket: aws.String(bucket),
+			Delete: &types.Delete{
+				Objects: objectsToDelete,
+				Quiet:   aws.Bool(true),
+			},
+		}
+
+		_, err = s3Client.DeleteObjects(context.TODO(), deleteObjectsInput)
+		if err != nil {
+			return fmt.Errorf("failed to delete objects: %w", err)
+		}
+
+		if listObjectsOutput.IsTruncated != nil && *listObjectsOutput.IsTruncated {
+			continuationToken = listObjectsOutput.NextContinuationToken
+		} else {
+			break
+		}
+	}
+	return nil
+}
+
+func main() {
+
+	var connections []remoteConnection
+
+	fileBytes, err := os.ReadFile("./connections.json")
+	if err != nil {
+		log.Fatal("Error reading connections.json: ", err)
+	}
+
+	err = json.Unmarshal(fileBytes, &connections)
+	if err != nil {
+		log.Fatal("error unmarshaling: ", err)
+	}
+
+	for _, connection := range connections {
+
+		if !connection.Active {
+			continue
+		}
+
+		awsConfig := getAwsConfig()
+		s3Client := s3.NewFromConfig(awsConfig)
+
+		// get folders for 12 months
+		month := time.Now()
+		month = month.AddDate(0, -1, 0)
+		for x := 0; x < 12; x++ {
+
+			month = month.AddDate(0, -1, 0)
+			entryBucket := fmt.Sprintf("entry/%s/", month.Format("2006-01"))
+			fmt.Println("entryBucket: ", entryBucket)
+
+			deleteFolder(s3Client, connection.Bucket, entryBucket)
+		}
+
+	}
+
+}
---
+++ readme.txt
@@ -0,0 +1,2 @@
+A simple go program to delete old images from buckets.
+Buckets to be cleared are listed in a json file.
